43cdcb23bc42d3666b8af30a5a6e63b21f73ad26
xWSWx <wsw.93@yandex.ru>
2024-07-16 13:19:58 +0300

    Валидация параметра, наличия файла, класс сессии

--- siisltd.ru/siisltd.ru/Program.cs
+++ siisltd.ru/siisltd.ru/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ProgramNameSpace.Session;
+namespace ProgramNameSpace
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: programm.exe <path-to-csv>");
+                return;
+            }
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine($"file {args[0]} not exists");
+                return;
+            }
+
+            var targetPath = args[0];
+
+
+        }
+    }
+}
---
+++ siisltd.ru/siisltd.ru/Session.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramNameSpace.Session
+{
+    public enum SessionState
+    {
+        Pause, Ready, Talk, Processing, CallBack
+    }
+    public class Session
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime FinishTime { get; set; }
+        public string? ProjectName { get; set; }
+        public string? OperatorName { get; set; }
+        public SessionState State { get; set; }
+        public TimeSpan Duration { get; set; }
+        ////////////////
+        //// Скроем пока что
+        private Session() { }
+
+        public static Session CreateSession(String source)
+        {
+            var columns = source.Split(';');
+
+            if (!DateTime.TryParse(columns[0], out DateTime startTime))
+            {
+                throw new ArgumentException($"cant parse {columns[0]} to DateTime");
+            }
+            if (!DateTime.TryParse(columns[1], out DateTime finishTime))
+            {
+                throw new ArgumentException($"cant parse {columns[1]} to DateTime");
+            }
+            var projectName = columns[2];
+            var operatorName = columns[3];
+            if (!Enum.TryParse(columns[4], out SessionState state)) 
+            {
+                throw new ArgumentException($"cant parse {columns[4]} to SessionState");
+            }
+            if (TimeSpan.TryParse(columns[5], out TimeSpan duration)) 
+            {
+                throw new ArgumentException($"cant parse {columns[5]} to Duration");
+            }
+
+            return new ()
+            {
+                StartTime = startTime,
+                FinishTime = finishTime,
+                ProjectName = projectName,
+                OperatorName = operatorName,
+                State = state,
+                Duration = duration
+            };
+        }
+    }
+}
